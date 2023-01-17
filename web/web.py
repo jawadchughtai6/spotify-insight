4f2f2c32389810f0b2ea6bc5d492f1045917439b
AhmedBuksh <ahmed.buksh26@gmail.com>
2023-01-17 14:56:48 +0500

    :rocket: web module refactored

--- web/web.py
+++ web/web.py
@@ -1,50 +1,51 @@
-import sys
 import datetime
-
-sys.path.append("..")
-
+import sys
+from peewee import fn
+from playhouse.shortcuts import model_to_dict
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
-from peewee import *
-from playhouse.shortcuts import model_to_dict
 
-from db.models import User, Recommendation
 from application import Application
+from db.models import User, Recommendation
 
-app = Application('Compute Insight')
-
-db = PostgresqlDatabase(app.env('DB'),
-                        user=app.env('DB_USER'),
-                        password=app.env('DB_PASSWORD'),
-                        host=app.env('DB_HOST'),
-                        port=app.env('DB_PORT'))
+config = Application("Compute Insight")
 
 
 def user_recommendations(request):
-    username = request.path_params['username']
-    date = request.query_params['date'] if 'date' in request.query_params else None
+    """
+    Get request for fetching spotify recommendations against provided username and
+    the date for which information is required.
+    """
+    username = request.path_params["username"]
+    date = request.query_params["date"] if "date" in request.query_params else None
 
-    if date:
-        date = datetime.datetime.strptime(date, '%Y-%m-%d')
-    else:
-        date = datetime.datetime.now().date()
+    try:
+        date = datetime.datetime.strptime(date, "%Y-%m-%d") if date else datetime.datetime.now().date()
+    except ValueError as ve:
+        return JSONResponse({"Error": "Kindly provide valid date format"})
 
-    with db:
-        recommendations = Recommendation.select(Recommendation.id,
-                                                Recommendation.track_title,
-                                                Recommendation.track_id,
-                                                Recommendation.track_link,
-                                                Recommendation.user) \
-            .join(User) \
-            .where((User.username == username) &
-                   (fn.date_trunc('day', Recommendation.created_at) == date))
+    with config.db_session("recommendations"):
+        recommendations = (
+            Recommendation.select(
+                Recommendation.id,
+                Recommendation.track_title,
+                Recommendation.track_id,
+                Recommendation.track_link,
+                Recommendation.user
+            )
+            .join(User)
+            .where(
+                (User.username == username)
+                & (fn.date_trunc("day", Recommendation.created_at) == date)
+            )
+        )
 
     return JSONResponse([model_to_dict(item) for item in recommendations])
 
 
 routes = [
-    Route('/user-recommendations/{username}', user_recommendations),
+    Route("/user-recommendations/{username}", user_recommendations),
 ]
 
 app = Starlette(debug=True, routes=routes)
